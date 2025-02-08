a8789b5f64843d33b90fd1b0c2318dc5ee29664a
redz <hehiaha2@gmail.com>
2025-02-08 22:29:47 +0700

    Create yo.lua

---
+++ yo.lua
@@ -0,0 +1,6 @@
+FruitsToReset = {  -- << Fruits it'll reset with, so you can put these fruits in trade to counter value difference
+    "Quake-Quake",
+    "Love-Love",
+    "Magma-Magma",
+    "Ghost-Ghost",
+    "Barrier-Barrier",
